ef0dab846becdcfe6d0d1441371d8b06236d25e6
Ilkiv-Yaroslav <94923947+Ilkiv-Yaroslav@users.noreply.github.com>
2021-12-10 08:45:37 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# PP
+# LabAP
+python 3.8.2 virtualenv+requirements.txt
+Запуск сервера
+--port=5000
+http://127.0.0.1:5000/api/v1/hello-world-13
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/hello-world-13")
+def hello_world():
+    return 'Hello World 13'
+
+if __name__=='__main__':
+    app.run()
+
+# http://127.0.0.1:5000/api/v1/hello-world-13
