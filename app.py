43d069e852281beb9ff4195500cf5fcdf487a405
Ilkiv-Yaroslav <94923947+Ilkiv-Yaroslav@users.noreply.github.com>
2022-11-28 08:01:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/api/v1/hello-world-13")
+@app.route("/api/v1/hello-world-11")
 def hello_world():
     return 'Hello World 13'
 
 if __name__=='__main__':
     app.run()
 
-# http://127.0.0.1:5000/api/v1/hello-world-13
+# http://127.0.0.1:5000/api/v1/hello-world-11
